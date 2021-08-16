1543c04fbf97799781aaa16b86ced13cfe44f1f5
Kristina Savova <43776504+krissisavova@users.noreply.github.com>
2021-08-16 19:18:03 +0300

    deleting the fantastic 4 and making all char*'s std::string's

--- User.cpp
+++ User.cpp
@@ -1,63 +1,43 @@
 #include "User.h"
 #include <iostream>
 #include <cstring>
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
 User::User() {
-	username = nullptr;
-	password = nullptr;
-	emailAddress = nullptr;
+	username = "\0";
+	password = "\0";
+	emailAddress = "\0";
 }
 
-void User::del() {
-	delete[] username;
-	delete[] password;
-	delete[] emailAddress;
-}
-void User::copy(const User& other) {
-
-	if (!other.username || !other.password || !other.emailAddress) {
-		std::cout << "\n\n\tERROR: The program has detected a nullptr!\n";
-		return;
-	}
-
-	username = new char[strlen(other.username) + 1];
-	strcpy_s(username, strlen(other.username) + 1, other.username);
-
-	password = new char[strlen(other.password) + 1];
-	strcpy_s(password, strlen(other.password) + 1, other.password);
-
-	emailAddress = new char[strlen(other.emailAddress) + 1];
-	strcpy_s(emailAddress, strlen(other.emailAddress) + 1, other.emailAddress);
-}
-
-User::User(const User& other) {
-	copy(other);
-}
-
-User::~User() {
-	del();
-}
 
 void User::registrationInput() {
-		cout << "\n Enter a username: ";
-		cin >> username;
-		cout << "\n Enter an email address: ";
-		cin >> emailAddress;
-		cout << "\n Enter a password: ";
-		cin >> password;
-		cout << "\n Repeat your password: ";
-		cin >> password;
+	cout << "Enter a username: ";
+	getline(cin, username);
+
+	cout << "Enter a password: ";
+	getline(cin, password);
+
+	cout << "Enter an email address: ";
+	getline(cin, emailAddress);
 }
 
 void User::loginInput() {
-	cout << "\n Enter your username: ";
-	cin >> username;
-	cout << "\n Enter your password: ";
-	cin >> password;
-	cout << "\n Enter your email address: ";
-	cin >> emailAddress;
+	cout << "Enter your username: ";
+	getline(cin, username);
+
+	cout << "Enter your password: ";
+	getline(cin, password);
+
+	cout << "Enter your email address: ";
+	getline(cin, emailAddress);
+}
+
+void User::printInfo() const {
+	cout << endl;
+	cout << "Your name is " << username << endl;
+	cout << "Your email is " << emailAddress << endl;
 }
