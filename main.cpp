f376bbf65759ed02aabfbfa5282f7287c080b470
Kristina Savova <43776504+krissisavova@users.noreply.github.com>
2021-08-16 19:16:24 +0300

    fixing the switch loop
    TODO: adding validations for username, password and email address; could add a "change password" functionality

--- main.cpp
+++ main.cpp
@@ -12,24 +12,26 @@ void welcomeText() {
 	cout << "***********************************************************************************************\n" << endl;
 
 	cout << "1. Sign up / Register " << endl;
-	cout << "2. Already have an account? Login " << endl;
+	cout << "2. Sign in / Login " << endl;
 }
 
 int main() {
 
+	User u1, u2;
 
 	int option = 0;
 	welcomeText();
-	cout << "Choose an option: " << endl;
-	cin >> option;
+	cout << "Choose an option: ";
+	cin >> option;	
 
-		system("cls");
+	system("cls");
+	cin.ignore(); // ignoring that enter the user presses when inserts the option number
 
-		//switch (option) {
-		//case 1: registrationInput(); break;
-		//case 2: cout << "Hello!\n"; break;
-		//default: cout << "Invalid option! Exiting..."; break;
-		//}
+	switch (option) {
+	case 1: u1.registrationInput(); u1.printInfo(); break;
+	case 2: u1.loginInput(); u1.printInfo(); break;
+	default: cout << "Invalid option! Exiting..."; break;
+	}
 
 	return 0;
 }
