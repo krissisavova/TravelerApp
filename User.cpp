123d020829bdff7ea8b51afc7cf59b89a124addd
Kristina Savova <43776504+krissisavova@users.noreply.github.com>
2021-08-18 14:49:24 +0300

    editing reg and login functions
    moving some code from main to user.cpp, including some assert validations for username, password and email address
    TODO: fixing overriden info issue coming from the reg function

--- User.cpp
+++ User.cpp
@@ -1,7 +1,10 @@
 #include "User.h"
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <string>
+#include <cassert>
+#include <regex>
 
 using std::cin;
 using std::cout;
@@ -25,18 +28,39 @@ std::string User::getEmailAddress() const {
 	return emailAddress;
 }
 
-void User::registrationInput() {
-	cout << "Enter a username: ";
-	getline(cin, username);
+void User::registrationInput(std::fstream& file) {
+	file.open("users.db", std::ios::out); // write mode 
+	if (!file) {
+		cout << "Failed to open file...\n";
+		exit(1);
+	}
+	else {
+		file << "Usernames:" << "\t" << "Passwords:" << "\t" << "Email Addresses:" << endl;
 
-	cout << "Enter a password: ";
-	getline(cin, password);
+			cout << "Enter a username: ";
+			getline(cin, username);
+			assert(username.size() > 2 && username.size() < 20);
 
-	cout << "Enter an email address: ";
-	getline(cin, emailAddress);
+			cout << "Enter a password: ";
+			getline(cin, password);
+			assert(password.size() > 5 && password.size() < 15);
+
+			const std::regex pattern
+			("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+
+			cout << "Enter an email address: ";
+			getline(cin, emailAddress);
+			assert(std::regex_match(emailAddress, pattern));
+
+		file << username << "\t" << password << "\t" << emailAddress << endl;
+
+		file.close();
+	}
 }
 
-void User::loginInput() {
+bool User::isLoggedIn() {
+	std::string un, pwd, ea;
+
 	cout << "Enter your username: ";
 	getline(cin, username);
 
@@ -45,4 +69,17 @@ void User::loginInput() {
 
 	cout << "Enter your email address: ";
 	getline(cin, emailAddress);
+
+	std::ifstream read("users.db");
+	while (read.good()) {
+		getline(read, un);
+		getline(read, pwd);
+		getline(read, ea);
+	}
+
+	if (un == username && pwd == password && ea == emailAddress) 
+		return true;
+	else 
+		return false;
 }
+
